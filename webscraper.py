1cb1cd221d672cdc9fb7011cd187a75b9fdcd35c
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2021-10-08 22:53:43 -0700

    finished core functionality for steam

--- webscraper.py
+++ webscraper.py
@@ -1,15 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-steamUrl = "https://store.steampowered.com/search/?term="
-winGameUrl = "https://www.wingamestore.com/search/?SearchWord="
-GOGUrl = "https://www.gog.com/games?search="
-humbleUrl = "https://www.humblebundle.com/store/search?sort=bestselling&search="
+steamUrl = 'https://store.steampowered.com/search/?term='
+winGameUrl = 'https://www.wingamestore.com/search/?SearchWord='
+GOGUrl = 'https://www.gog.com/games?search='
+humbleUrl = 'https://www.humblebundle.com/store/search?sort=bestselling&search='
 
-searchTerm = "children of morta"
+stringInput = 'chILDREN OF morta' #user input here
+searchTerm = stringInput.lower()
 
-steamPage = requests.get(steamUrl)
+page = requests.get(steamUrl + searchTerm)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id = "search_resultsRows")
 
-print(soup)
+if (results != None):
+    res = results.find_all("a")
+    gameTitle = res[0].find("span", class_ = "title").string.lower()
+
+    if (gameTitle == searchTerm):
+        originalPrice = res[0].find("strike").string
+
+        discountDiv = res[0].find("div", class_ = "col search_price discounted responsive_secondrow")
+        discountDiv.span.extract()
+        discountDiv.br.extract()
+        discountedPrice = discountDiv.text
+
+        pctDiv = res[0].find("div", class_ = "col search_price_discount_combined responsive_secondrow")
+        discountPct = pctDiv.find("span").string
+
+        # output here
+        print("original price -->", originalPrice)
+        print("discounted price -->", discountedPrice)
+        print("discount -->", discountPct)
+    else:
+        print('WRONG GAME') # output
